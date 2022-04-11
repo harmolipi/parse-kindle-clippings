d7011aee992f4c7fdb8148decd51ada2b6e077fb
harmolipi <nikob381@gmail.com>
2022-04-11 14:38:56 -0400

    Use Context API to handle books

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import AppContext from '../components/AppContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [clippings, setClippings] = useState([]);
+  const [authors, setAuthors] = useState([]);
+  const [books, setBooks] = useState([]);
+  return (
+    <AppContext.Provider
+      value={{ clippings, setClippings, authors, setAuthors, books, setBooks }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -9,32 +9,32 @@ import {
   organizeKindleEntriesByAuthors,
 } from '@darylserrano/kindle-clippings';
 import ClippingsInput from '../components/ClippingsInput';
-import DisplayAuthors from '../components/DisplayAuthors';
+import DisplayBooks from '../components/DisplayBooks';
 import Link from 'next/link';
+import { useContext } from 'react';
+import AppContext from '../components/AppContext';
 
 export default function Home() {
-  const [clippings, setClippings] = useState([]);
-  const [books, setBooks] = useState([]);
-  const [authors, setAuthors] = useState([]);
-  const [authorList, setAuthorList] = useState();
+  const value = useContext(AppContext);
 
   const handleClippingChange = (e) => {
     if (e.target.value === '') {
-      setClippings([]);
-      setBooks([]);
-      setAuthors([]);
-      setAuthorList();
+      value.setClippings([]);
+      value.setBooks([]);
+      value.setAuthors([]);
     } else {
       try {
-        const clippings = readKindleClipping(e.target.value);
-        const parsedClippings = parseKindleEntries(clippings);
-        const parsedBooks = organizeKindleEntriesByBookTitle(parsedClippings);
+        const clippingsInput = readKindleClipping(e.target.value);
+        const parsedClippings = parseKindleEntries(clippingsInput);
+        const parsedBooks = Object.fromEntries(
+          organizeKindleEntriesByBookTitle(parsedClippings)
+        );
         const parsedAuthors = Object.fromEntries(
           organizeKindleEntriesByAuthors(parsedClippings)
         );
-        setClippings(parsedClippings);
-        setBooks(parsedBooks);
-        setAuthors(parsedAuthors);
+        value.setClippings(parsedClippings);
+        value.setBooks(parsedBooks);
+        value.setAuthors(parsedAuthors);
       } catch (err) {
         console.log(err);
       }
@@ -64,14 +64,14 @@ export default function Home() {
           <main className="main">
             <ClippingsInput handleChange={handleClippingChange} />
             <div className="field">
-              <label className="label">Output</label>
+              <label className="label">JSON</label>
               <div className="control">
                 <textarea
                   className="textarea"
                   placeholder="Output will appear here"
                   value={
-                    clippings.length > 0
-                      ? JSON.stringify(clippings, null, 2)
+                    value.clippings.length > 0
+                      ? JSON.stringify(value.clippings, null, 2)
                       : ''
                   }
                   readOnly
@@ -82,15 +82,14 @@ export default function Home() {
         </div>
       </div>
       <div className="section">
-        {/* Display list of authors with links to '/authors/[author-name]' if there are any books*/}
         <div className="container">
           <main className="main">
             <h1 className="title is-1 has-text-centered">Books</h1>
             <p className="subtitle is-4 has-text-centered">
-              Click on an author to see a list of his books.
+              Click on a book to see its highlights.
             </p>
             <div className="columns is-multiline">
-              <DisplayAuthors authors={authors} />
+              <DisplayBooks books={value.books} />
             </div>
           </main>
         </div>
